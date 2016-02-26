80492cb9cafb0c502520fefba0bacae664ac8391
William Zhuang <wzhuang1324@gmail.com>
2016-02-26 13:21:29 -0800

    Initial commit

---
+++ thankyouevents.php
@@ -0,0 +1,13 @@
+<?
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_message = "
+Name: ".$name."
+Email: ".$email."
+Message: ".$message."
+";
+mail ( "fstatcal@gmail.com", "New FST inquiry", $email_message);
+header("Location: events.html");
+?>
